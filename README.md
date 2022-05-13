adb81b553d525a018234b0b140c56106f08f88bf
Parham-develope <parhamtaherkhany@gmail.com>
2022-05-13 21:28:06 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-functional-components-example-bzqd3v
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-functional-components-example-bzqd3v)
---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+class HelloWorld extends React.Component {
+  render() {
+    return <div>
+      
+    </div>;
+  }
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
